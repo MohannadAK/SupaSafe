9ef3bfef504c3a2c874c8e97532f3af6a15cd1c0
MohannadAK <mohannad.a.karim@gmail.com>
2025-05-21 00:11:52 +0300

    Using bcryptjs instead of bcrypt to be compatible with the docker container

--- Server/src/services/cryptoService.js
+++ Server/src/services/cryptoService.js
@@ -4,7 +4,7 @@
  */
 
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 // Constants for cryptographic operations
